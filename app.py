6d9838bd2e308d0930979303dc2309f9bcd54c14
Maria Eduarda <madumgduda@gmail.com>
2024-12-16 15:24:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ if "rendimentos" not in st.session_state:
     st.session_state["rendimentos"] = []
 if "despesas" not in st.session_state:
     st.session_state["despesas"] = []
+if "impostos" not in st.session_state:
+    st.session_state["impostos"] = []
 
 def formatar_data_brasil(data):
     meses = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
@@ -53,6 +55,27 @@ def cadastro_despesas():
         st.success(f'Despesa de {valor_formatado} adicionada com sucesso para {data_formatada}')
 
 
+def cadastro_impostos():
+    st.subheader("Cadastro de Impostos")
+    nome = st.text_input("Nome do imposto")
+    if not nome.strip():
+        st.error("O campo 'Nome' não pode estar vazio.")
+    data = st.date_input("Data de pagamento", value=datetime.now(), format="DD/MM/YYYY")
+    data_formatada = formatar_data_brasil(data)
+    valor = st.number_input("Valor do Imposto", min_value=0.0)
+    if valor == 0.0:
+        st.error("O campo 'Valor' não pode estar vazio.")
+    valor_formatado = formatar_valor_brasil(valor)
+    st.text(f"Valor formatado: {valor_formatado}")
+    vencimento = st.text_input("Data de vencimento do imposto")
+    if not vencimento.strip():
+        st.error("O campo 'Vencimento' não pode estar vazio.")
+    if st.button("Adicionar Rendimento"):
+        novo_imposto = {"nome": nome, "data": data_formatada, "valor": valor, "vencimento": vencimento, }
+        st.session_state["impostos"].append(novo_imposto)
+        st.success(f'Imposto de {valor_formatado} adicionado com sucesso para {data_formatada}')
+
+
 def visualizar_ou_excluir_rendimentos():
     st.subheader("Seus Rendimentos")
     for i, rendimento in enumerate(st.session_state["rendimentos"]):
@@ -82,6 +105,24 @@ def visualizar_ou_excluir_despesas():
         with col4:
             if st.button("Excluir", key=f'delete_desp_{i}'):
                 st.session_state["despesas"].pop(i)
+                #st.experimental_rerun()
+
+
+def visualizar_ou_excluir_impostos():
+    st.subheader("Seus Impostos")
+    for i, imposto in enumerate(st.session_state["impostos"]):
+        col1, col2, col3, col4, col5 = st.columns([2, 2, 2, 2, 1])
+        with col1:
+            st.write(f"**Valor:** {imposto['valor']}")
+        with col2:
+            st.write(f"**Data:** {imposto['data']}")
+        with col3:
+            st.write(f"**Vencimento:** {imposto['vencimento']}")
+        with col4:
+            st.write(f"**Nome:** {imposto['nome']}")
+        with col5:
+            if st.button("Excluir", key=f'delete_rend_{i}'):
+                st.session_state["impostos"].pop(i)
                 #st.experimental_rerun()
 
 
@@ -119,13 +160,16 @@ def visualizar_gráfico_rendimento():
     
 
 st.title("Você no Comando - Controle Financeiro para Autônomos")
-menu = st.sidebar.selectbox("Selecione uma opção", ["Cadastrar Rendimento", "Cadastrar Despesa", "Visualizar Dados"])
+menu = st.sidebar.selectbox("Selecione uma opção", ["Cadastrar Rendimento", "Cadastrar Despesa", "Cadastrar Imposto", "Visualizar Dados"])
 
 if menu == "Cadastrar Rendimento":
     cadastro_rendimentos()
 elif menu == "Cadastrar Despesa":
     cadastro_despesas()
+elif menu == "Cadastrar Imposto":
+    cadastro_impostos()
 elif menu == "Visualizar Dados":
     visualizar_ou_excluir_rendimentos()
     visualizar_ou_excluir_despesas()
+    visualizar_ou_excluir_impostos()
     visualizar_gráfico_rendimento()
