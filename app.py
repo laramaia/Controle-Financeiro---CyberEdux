c49d95ff8dbd27932cdb9cddca3e369a2383b398
Maria Eduarda <madumgduda@gmail.com>
2024-12-16 15:57:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def visualizar_grafico_despesa():
     
 
 st.title("Você no Comando - Controle Financeiro para Autônomos")
-menu = st.sidebar.selectbox("Selecione uma opção", ["Cadastrar Rendimento", "Cadastrar Despesa", "Cadastrar Imposto", "Visualizar Dados"])
+menu = st.sidebar.selectbox("Selecione uma opção", ["Cadastrar Rendimento", "Cadastrar Despesa", "Cadastrar Imposto", "Cadastrar Meta", "Visualizar Dados"])
 
 if menu == "Cadastrar Rendimento":
     cadastro_rendimentos()
