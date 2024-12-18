6d52df4a07a0b5f30be9a2b93c7d972b937b08f3
Maria Eduarda <madumgduda@gmail.com>
2024-12-17 21:56:32 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Você no Comando", page_icon=":bar_chart:")
+    page_title="Você no Controle", page_icon=":bar_chart:")
 
-st.title("Você no Comando - Controle Financeiro para Autônomos")
+st.title("Você no Controle - Controle Financeiro para Autônomos")
 
-st.subheader("Você no comando! Te auxiliando a estar no comando de suas finanças!")
-st.write("O programa Você no  comando pode te ajudar a gerenciar suas finanças, para que saiba para onde vai seu dinheiro.")
+st.subheader("Você no controle! Te auxiliando a estar no controle de suas finanças!")
+st.write("O programa Você no  controle pode te ajudar a gerenciar suas finanças, para que saiba para onde vai seu dinheiro.")
 st.write("O programa foi criado  por Lara Caroline, Raquel Arantes e Maria Eduarda de Gouveia")
-st.page_link("app/Você no comando.py", label= "Você no Comando")
+st.page_link("app/Você no controle.py", label= "Você no Controle")
