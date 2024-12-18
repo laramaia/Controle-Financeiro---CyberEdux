3c52b088ee355dd6a8e7b42e8568dc5019d32cc1
Lara <laracarolinemaya@gmail.com>
2024-12-18 00:16:45 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.set_page_config(
 
 st.title("Você no Controle - Controle Financeiro para Autônomos")
 
-st.subheader("Você no controle! Te auxiliando a estar no controle de suas finanças!")
-st.write("O programa Você no  controle pode te ajudar a gerenciar suas finanças, para que saiba para onde vai seu dinheiro.")
-st.write("O programa foi criado  por Lara Caroline, Raquel Arantes e Maria Eduarda de Gouveia")
-st.page_link("app/Você no controle.py", label= "Você no Controle")
+st.subheader("Você no Controle: Simplificando a gestão das suas finanças!")
+st.write("O programa tem como objetivo ajudar você a entender e gerenciar suas finanças para tomada de decisões mais conscientes.")
+st.write("Desenvolvido  por Lara Caroline, Raquel Arantes e Maria Eduarda de Gouveia.")
+
